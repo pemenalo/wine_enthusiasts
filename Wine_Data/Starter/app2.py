3adf28a9614383835a4e9c9a4b23a86d8ed0ab67
lorinalexd@gmail.com <lorinalexd@gmail.com>
2022-03-04 21:07:09 -0500

    create prediction function

--- Wine_Data/Starter/app2.py
+++ Wine_Data/Starter/app2.py
@@ -2,19 +2,20 @@
 import os
 import sys
 from flask import Flask, render_template, request
-import tensorflow as tf
-from tensorflow import keras
+# import tensorflow as tf
+# from tensorflow import keras
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 # from keras.models import load_model
 from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.models import Model
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.models import Model
+# from tensorflow.keras.models import load_model
 import numpy as np
 from joblib import dump, load
 from sklearn.ensemble import RandomForestClassifier
+from utils import prediction
 
 app = Flask(__name__)
 
@@ -49,42 +50,11 @@ def submit():
 
 		# call preprocessDataAndPredict and pass inputs
 		try:
-			# prediction = preprocessDataAndPredict(acidity, volacidity, citric, res_sugar, chlorides, so2,tso2,density, pH,sulphates,alcohol)
-			# ----
-
-			# keep all inputs in array
-			test_data = [acidity, volacidity, citric, res_sugar,
-				chlorides, so2, tso2, density, pH, sulphates, alcohol]
-			print(test_data)
-
-	# convert value data into numpy array
-			# test_data = np.array(test_data)
-
-	# reshape array
-			# test_data = test_data.reshape(1, -1)
-			print(test_data)
-
-	# #load trained model
-
-			# model = load_model('Wine_Enthusiast_Optimization_r_2.h5')
-			model = load('Wine_Enthusiast_RF_r.joblib')
-			print("Model Loaded")
-			# test_data = preprocess_input(test_data, mode='tf')
-	# predict
-			from sklearn.preprocessing import StandardScaler
-			scaler = StandardScaler()
-			
-			prediction = model.predict(scaler.transform([test_data]))
-			# scaler.transform([clean_df_r.iloc[3,0:11].to_list()])
-			prediction = str(prediction).lstrip('[').rstrip(']')
-			print(f"Prediction Value: {prediction}")
-			print(scaler.transform(test_data))
-
-
-			# ----
+			label=prediction(acidity, volacidity, citric, res_sugar,
+        chlorides, so2, tso2, density, pH, sulphates, alcohol)
 
 		 # .py to HTML
-			return render_template("submit.html",prediction = prediction)
+			return render_template("submit.html",label = label)
 
 		except ValueError:
 			return "Please Enter valid values"
